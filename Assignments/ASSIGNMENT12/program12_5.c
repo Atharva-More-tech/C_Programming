9c0328b4743074851d640d42a4c67d65f35bd60d
Atharva Sanjay More <atharva.more.tech@gmail.com>
2025-11-07 00:12:28 +0530

    Assignment12

---
+++ Assignments/ASSIGNMENT12/program12_5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+////////////////////////////////////////////////
+//
+// Function Name: is_divisible_by_five
+// Description: Check if number is divisible by 5 
+// Input: int
+// Output : boolean
+// Author : Atharva Sanjay More
+// Date : 2/11/25
+//
+////////////////////////////////////////////////
+bool is_divisible_by_five(int number)
+{
+ if(number % 5 ==0)
+ {
+    return true;
+ }
+ else
+ {
+ return false;
+}
+}           //Time Complexity= O(1)
+int main(void)
+{
+    int number;
+
+    printf("Enter number : ");
+    scanf("%d", &number);
+
+    printf("%s\n", is_divisible_by_five(number) ? "Yes" : "No");
+
+    return 0;
+}
