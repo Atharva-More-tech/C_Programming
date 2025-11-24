8c07f1e10086e657a32301f3d11a39415e27b071
Atharva Sanjay More <atharva.more.tech@gmail.com>
2025-11-25 00:35:11 +0530

    Add files via upload

---
+++ Assignments/Assignment31/program31_4.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+//////////////////////////
+//
+// Function Name: Pattern
+// Description: display pattern.
+// INPUT : int
+// OUTPUT : void
+// Author : Atharva Sanjay More
+// Date : 25/11/25
+//
+//////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j =0;
+
+    for(i = 1 ; i <= iRow; i++ )
+    {
+        for(j = 1 ; j <= iCol; j++)
+        {
+            if( i == 1 || i == iRow  || j == 1 || j == iCol || i == j )
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("\t");
+            }
+        }
+        
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter the number of row :");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of column :");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
