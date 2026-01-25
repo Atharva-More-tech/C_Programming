e07fa27b29e75b15dc4489418910a99406b77672
Atharva Sanjay More <atharva.more.tech@gmail.com>
2026-01-25 22:59:09 +0530

    Add files via upload

---
+++ Assignments/Assignment48/program48_5.c
@@ -0,0 +1,98 @@
+// Description: Display Elements at Odd Positions
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define True 1
+#define False 0
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE Head , int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Next = NULL;
+    newn->Data = no;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        *Head = newn;
+    }
+}
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+    printf("NULL \n");
+}
+
+void DisplayOddPosition(PNODE Head)
+{
+    int iPos = 0;
+    
+    while(Head != NULL)
+    {
+        iPos++;
+        if(iPos % 2 != 0)
+        {
+            printf("| %d |->",Head->Data);
+        }
+        Head = Head->Next;
+    } 
+    
+}
+
+int Count(PNODE Head)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        iCount++;
+        Head = Head->Next;
+    }
+    return iCount;
+}
+
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE First = NULL;
+    InsertFirst(&First, 650);
+    InsertFirst(&First, 40);
+    InsertFirst(&First, 393);
+    InsertFirst(&First, 230);
+    InsertFirst(&First, 11);
+
+    Display(First);
+
+    iRet = Count(First);
+
+    printf("Number of elements in node are : %d\n",iRet);
+
+    DisplayOddPosition(First);
+    
+    return 0;
+} 
